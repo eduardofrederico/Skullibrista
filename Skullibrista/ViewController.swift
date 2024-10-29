e8000163a4e27fbf61eb3991f2baa62163f88a9a
Eduardo Frederico <eduardo.frederico@icloud.com>
2024-10-29 20:51:45 -0300

    Initial Commit

---
+++ Skullibrista/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Skullibrista
+//
+//  Created by Eduardo Frederico on 29/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SkullibristaTests/SkullibristaTests.swift
@@ -0,0 +1,17 @@
+//
+//  SkullibristaTests.swift
+//  SkullibristaTests
+//
+//  Created by Eduardo Frederico on 29/10/24.
+//
+
+import Testing
+@testable import Skullibrista
+
+struct SkullibristaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
